3b454239298f9272f3f60ea2a075bc3b0a002606
Elliot Zayas <ezayas1@gmail.com>
2026-01-18 23:44:10 -0500

    Added users import to main,py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.database import connect_db
 from app.routers import appointments, patients, messages, clinics
+from app.routers import users
+
 
 app = FastAPI(title="Empathy AI Backend")
 
@@ -12,6 +14,8 @@ app.include_router(appointments.router)
 app.include_router(patients.router)
 app.include_router(messages.router)
 app.include_router(clinics.router)
+app.include_router(users.router)
+
 
 @app.get("/")
 def root():
